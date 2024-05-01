04e4a33c677a8bf4c99f0f324daa0c78dae07625
DESKTOP-871B27E\swaro <pallabipanigrahi29@gmail.com>
2024-05-01 09:39:12 +0100

    first commit

---
+++ src/main/java/com/pallabi/schedule/JsonSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.pallabi.schedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pallabi/schedule/JsonSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pallabi.schedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
